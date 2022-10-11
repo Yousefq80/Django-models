2df758d20fa8788cd7cd45894e7fb46107ef4e7e
Yousefq80 <ysalharby@gmail.com>
2022-10-11 09:59:51 +0300

    fix default error

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from datetime import datetime
 class Restaurant(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(defaults="")
+    description = models.CharField(max_length=30,default='')
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     created_at = models.DateTimeField(default=datetime.now(), blank=True)
