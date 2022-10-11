0efc4189e1f8397d639aca465202c929cdba7f0b
Yousefq80 <ysalharby@gmail.com>
2022-10-11 08:11:42 +0300

    restaurant model is created

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(defaults="")
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateTimeField(default=datetime.now(), blank=True)
 
 # Create your models here.
