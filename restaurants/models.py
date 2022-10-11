7a7f35a1b3044daf29b9780e6ca09b92617d2ced
Yousefq80 <ysalharby@gmail.com>
2022-10-11 18:42:20 +0300

    Update models.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from datetime import datetime
 class Restaurant(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(default="")
+    description = models.TextField(default="")
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     created_at = models.DateTimeField(default=datetime.now(), blank=True)
